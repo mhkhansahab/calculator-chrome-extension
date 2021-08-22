8577f800c44b180d7fb274385a5b5381a8ae2eeb
Muhammad Hamza Khan <hamza.khansahab@gmail.com>
2021-08-22 14:50:12 +0500

    update 2,0

--- popup.js
+++ popup.js
@@ -1,2 +1,16 @@
-// Initialize button with user’s preferred color
+const elements = document.getElementsByClassName("num");
 
+function isOperator(op){
+    if(op === "+" && op === "-" && op === "x" && op === "/"){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+
+for(let i = 0; i < elements.length; i++){
+    elements[i].addEventListener("click", (e)=>{
+        console.log(e.target.innerHTML);
+    })
+}
