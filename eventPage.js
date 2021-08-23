f19066ea0aa1a05122799fc1d2ed93f286029e27
Muhammad Hamza Khan <hamza.khansahab@gmail.com>
2021-08-23 11:38:12 +0500

    calculator functionality added

---
+++ eventPage.js
@@ -0,0 +1,8 @@
+// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+//   if (request.todo === "showPageAction") {
+//     let queryOptions = { active: true, currentWindow: true };
+//     chrome.tabs.query(queryOptions, (tabs) => {
+//       chrome.pageAction.show(tabs[0].id);
+//     });
+//   }
+// });
--- popup.js
+++ popup.js
@@ -1,16 +1,59 @@
 const elements = document.getElementsByClassName("num");
+const result = document.getElementById("result");
+const reset = document.getElementById("reset-btn");
 
 function isOperator(op){
-    if(op === "+" && op === "-" && op === "x" && op === "/"){
+    if(op === "+" || op === "-" || op === "x" || op === "/"){
         return true;
     }else{
         return false;
     }
 }
 
+let arr=[];
 
 for(let i = 0; i < elements.length; i++){
     elements[i].addEventListener("click", (e)=>{
-        console.log(e.target.innerHTML);
+        const text = e.target.innerHTML;
+        if(!isOperator(text) && text !== "="){
+            if(arr.length === 0 || isOperator(arr[arr.length-1])){
+                arr.push(text);
+            }
+            else{
+                arr[arr.length-1] += text;
+            }
+        }
+        else if(text !== "="){
+            if(!isOperator(arr[arr.length-1])){
+                arr.push(text);
+            }
+        }else{
+            if(arr.length > 2){
+                let exp = parseFloat(arr[0]);
+                let count = 1;
+                while(count < arr.length){
+                    if(arr[count] === "+"){
+                        exp = exp + parseFloat(arr[count+1]);
+                    }
+                    else if(arr[count] === "-"){
+                        exp = exp - parseFloat(arr[count+1]);
+                    }
+                    else if(arr[count] === "x"){
+                        exp = exp * parseFloat(arr[count+1]);
+                    }
+                    else if(arr[count] === "/"){
+                        exp = exp / parseFloat(arr[count+1]);
+                    }
+                    count += 2;
+                    
+                }
+                arr = [exp];
+            }
+        }
+        result.innerHTML = arr.join(" ")
     })
 }
+reset.addEventListener("click",()=>{
+    arr=[];
+    result.innerHTML = arr;
+})
